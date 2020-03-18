179261dba5b8fdf51457485f2691d4c713a29db4
ASUS <ASUS@DESKTOP-3CABE86>
2020-03-18 01:07:07 +0100

    commit 18/03/2020 (1:06)

--- dari-spring-boot/src/main/java/tn/esprit/spring/services/FurnitureServiceImpl.java
+++ dari-spring-boot/src/main/java/tn/esprit/spring/services/FurnitureServiceImpl.java
@@ -1,10 +1,12 @@
 package tn.esprit.spring.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import tn.esprit.spring.entities.Furniture;
 import tn.esprit.spring.repository.FurnitureRepository;
 
+@Service
 public class FurnitureServiceImpl implements IFurnitureService{
 
 	@Autowired
