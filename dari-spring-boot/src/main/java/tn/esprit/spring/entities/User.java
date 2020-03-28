8a1ae142a32f05477113cb3e50d54735c5815ad2
oumyma houmrani <oumyma houmrani@oumyma>
2020-03-28 22:57:47 +0100

    here are the entites of oussama

--- dari-spring-boot/src/main/java/tn/esprit/spring/entities/User.java
+++ dari-spring-boot/src/main/java/tn/esprit/spring/entities/User.java
@@ -1,5 +1,8 @@
 package tn.esprit.spring.entities;
 
+
+
+
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Set;
@@ -10,30 +13,42 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
 @Entity
-@Table(name = "t_User")
-public class User implements Serializable{
-	private static final long serialVersionUID = 1L;
+public class User implements Serializable {
+	
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)	
 	private Long id;   
+	
 	
 	private String FirstName;
 	
 	private String Lastname;
 
-	@Temporal(TemporalType.DATE)
 	private Date dateNaissance;
-	
-	
+	private String Email;
+	private String Password;
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", FirstName=" + FirstName + ", Lastname=" + Lastname + ", dateNaissance="
+				+ dateNaissance + ", Email=" + Email + ", Password=" + Password + "]";
+	}
+	public User(Long id, String firstName, String lastname, Date dateNaissance, String email, String password) {
+		super();
+		this.id = id;
+		FirstName = firstName;
+		Lastname = lastname;
+		this.dateNaissance = dateNaissance;
+		Email = email;
+		Password = password;
+	}
 	
 	@OneToMany(cascade = CascadeType.ALL, mappedBy="user")
 	private Set<Ad> Ad;
-	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 	public Long getId() {
 		return id;
 	}
@@ -70,21 +85,62 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		Password = password;
 	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-	private String Email;
-	private String Password;
+	
 	@Override
-	public String toString() {
-		return "User [id=" + id + ", FirstName=" + FirstName + ", Lastname=" + Lastname + ", dateNaissance="
-				+ dateNaissance + ", Email=" + Email + ", Password=" + Password + "]";
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (Email == null) {
+			if (other.Email != null)
+				return false;
+		} else if (!Email.equals(other.Email))
+			return false;
+		if (FirstName == null) {
+			if (other.FirstName != null)
+				return false;
+		} else if (!FirstName.equals(other.FirstName))
+			return false;
+		if (Lastname == null) {
+			if (other.Lastname != null)
+				return false;
+		} else if (!Lastname.equals(other.Lastname))
+			return false;
+		if (Password == null) {
+			if (other.Password != null)
+				return false;
+		} else if (!Password.equals(other.Password))
+			return false;
+		if (dateNaissance == null) {
+			if (other.dateNaissance != null)
+				return false;
+		} else if (!dateNaissance.equals(other.dateNaissance))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
-	public User() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+	
+	
+	
+
+	
+	
 	
 	
 
 }
+
+	
+	
+	
+	
+	
+	
